60e9ea07606d7f791e6f927adbedcc9300dd2001
unknown <aditya20322@gmail.com>
2024-09-26 18:28:47 +0530

    code improvement

--- src/Components/pages/SignIn.js
+++ src/Components/pages/SignIn.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { SignInStyle } from './SignInStyle';
+import { FormStyle } from '../styles/FormStyle';
 import logo from "../img/logo.png";
 import { Link } from 'react-router-dom';
 
 export default function SignIn() {
   return (
-    <div className='SignIn' style={{...SignInStyle.style}}>
-      <div className='loginForm' style={{...SignInStyle.form}}>
-        <img className='SignInLogo' src={logo} alt="" style={{...SignInStyle.SigninLogo}}></img>
-        <div style={{...SignInStyle.div}}>
-            <input type='email' name='email' id='email' placeholder='Email' style={{...SignInStyle.input}}/>
+    <div className='SignIn' style={{...FormStyle.style}}>
+      <div className='loginForm' style={{...FormStyle.form}}>
+        <img className='SignInLogo' src={logo} alt="" style={{...FormStyle.Logo}}></img>
+        <div style={{...FormStyle.div}}>
+            <input type='email' name='email' id='email' placeholder='Email' style={{...FormStyle.input}}/>
         </div>
-        <div style={{...SignInStyle.div}}>
-            <input type='text' name='password' id='password' placeholder='Password' style={{...SignInStyle.input}}/>
+        <div style={{...FormStyle.div}}>
+            <input type='text' name='password' id='password' placeholder='Password' style={{...FormStyle.input}}/>
         </div>
-        <div style={{...SignInStyle.div}}>
-            <input type='submit'  id='submit-btn' value={"Sign In"} style={{...SignInStyle.input.submitBtn}}/>
+        <div style={{...FormStyle.div}}>
+            <input type='submit'  id='submit-btn' value={"Sign In"} style={{...FormStyle.input.submitBtn}}/>
         </div>
-        <div  className="form2" style={{...SignInStyle.input.form2}}>
+        <div  className="form2" style={{...FormStyle.input.form2}}>
             <p>Don't have an account? 
-              <Link to={"/signup"} style={{...SignInStyle.input.a}}>
+              <Link to={"/signup"} style={{...FormStyle.input.a}}>
               <span style={{color:"blue",cursor:"pointer"}}> SignUp</span>
               </Link>
             </p>
--- src/Components/pages/Signup.js
+++ src/Components/pages/Signup.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import logo from "../img/logo.png"
-import { SignUpStyle } from './SignUpStyle';
+import { FormStyle } from '../styles/FormStyle';
 import { Link } from 'react-router-dom';
+
 export default function Signup() {
   return (
-    <div className='SignUp' style={{...SignUpStyle.style}}>
-      <div className='form-container' style={{...SignUpStyle.form}}>
-        <img className='SignUpLogo' src={logo} alt="" style={{...SignUpStyle.SignupLogo}}></img>
-        <p className='loginPara' style={{...SignUpStyle.loginPara}}>SignUp to see photos and videos <br/> from your friends</p>
-        <div style={{...SignUpStyle.input.div}}>
-            <input type='email' name='email' id='email' placeholder='Email' style={{...SignUpStyle.input}}/>
+    <div className='SignUp' style={{...FormStyle.style}}>
+      <div className='form-container' style={{...FormStyle.form}}>
+        <img className='SignUpLogo' src={logo} alt="" style={{...FormStyle.Logo}}></img>
+        <p className='loginPara' style={{...FormStyle.loginPara}}>SignUp to see photos and videos <br/> from your friends</p>
+        <div style={{...FormStyle.input.div}}>
+            <input type='email' name='email' id='email' placeholder='Email' style={{...FormStyle.input}}/>
         </div>
-        <div style={{...SignUpStyle.input.div}}>
-            <input type='text' name='name' id='name' placeholder='FullName' style={{...SignUpStyle.input}}/>
+        <div style={{...FormStyle.input.div}}>
+            <input type='text' name='name' id='name' placeholder='FullName' style={{...FormStyle.input}}/>
         </div>
-        <div style={{...SignUpStyle.input.div}}>
-            <input type='text' name='username' id='username' placeholder='Username' style={{...SignUpStyle.input}}/>
+        <div style={{...FormStyle.input.div}}>
+            <input type='text' name='username' id='username' placeholder='Username' style={{...FormStyle.input}}/>
         </div>
-        <div style={{...SignUpStyle.input.div}}>
-            <input type='text' name='password' id='password' placeholder='Password' style={{...SignUpStyle.input}}/>
+        <div style={{...FormStyle.input.div}}>
+            <input type='text' name='password' id='password' placeholder='Password' style={{...FormStyle.input}}/>
         </div>
-        <p className='loginPara' style={{...SignUpStyle.loginPara,margin:"3px 0px",fontSize:"12px",marginTop:"18px"}}>By signing up , you are agreeing to <br/> instagram terms and policies.</p>
-        <div style={{...SignUpStyle.input.div}}>
-            <input type='submit'  id='submit-btn' value={"Sign Up"} style={{...SignUpStyle.input.submitBtn}}/>
+        <p className='loginPara' style={{...FormStyle.loginPara,margin:"3px 0px",fontSize:"12px",marginTop:"18px"}}>By signing up , you are agreeing to <br/> instagram terms and policies.</p>
+        <div style={{...FormStyle.input.div}}>
+            <input type='submit'  id='submit-btn' value={"Sign Up"} style={{...FormStyle.input.submitBtn}}/>
         </div>
-        <div  className="form2" style={{...SignUpStyle.input.form2}}>
+        <div  className="form2" style={{...FormStyle.input.form2}}>
             <p>Already have an account? 
-              <Link to={"/signin"} style={{...SignUpStyle.input.a}}>
+              <Link to={"/signin"} style={{...FormStyle.input.a}}>
               <span style={{color:"blue",cursor:"pointer"}}> SignIn</span>
               </Link>
             </p>
